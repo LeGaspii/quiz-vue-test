a7e07c5aaa43cf61c0e904f22b9d681a156693cc
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-04-27 13:10:33 +0200

    setup complet

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-        <b-nav>
-            <b-nav-item>Fancy Quiz of the Deadly Death</b-nav-item>
-            <b-nav-item>Counter: 8/10</b-nav-item>
+        <b-nav tabs>
+            <b-nav-item disabled><b>Fancy Quiz Of The Deadly Death</b></b-nav-item>
+            <b-nav-item disabled>Counter: 8/10</b-nav-item>
         </b-nav>
     </div>
 </template>
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>Coucou</h1>
+    <div class="question-boc-container">
+        <b-jumbotron>
+            <template #lead>
+                Une Question ?
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+            Réponses
+            </p>
+
+            <b-button variant="primary" href="#">Submit</b-button>
+            <b-button variant="success" href="#">Next Question</b-button>
+        </b-jumbotron>
+    </div>
 </template>
