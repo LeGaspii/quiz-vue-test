e4c9ecbd709898f0388315412c583dbb604082a2
Quentin Soubeyrol <legaspirateur@gmail.com>
2021-04-27 13:01:35 +0200

    bootstrap ok

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,3 +1,8 @@
 <template>
-    <h1>heyheyhey</h1>
+    <div>
+        <b-nav>
+            <b-nav-item>Fancy Quiz of the Deadly Death</b-nav-item>
+            <b-nav-item>Counter: 8/10</b-nav-item>
+        </b-nav>
+    </div>
 </template>
